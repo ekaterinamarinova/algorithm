c73972fc47c03ebf28404293c3f3f09b05bddb61
ekaterinamarinova <katherineeyall@gmail.com>
2021-01-05 01:04:32 +0200

    created an algorithm that finds the max hourglass in a 2d array

--- src/main/java/com/algorithm/ArrayManipulation.java
+++ src/main/java/com/algorithm/ArrayManipulation.java
@@ -6,8 +6,18 @@ public class ArrayManipulation {
 
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
+        int[][] twoDarr = new int[][]
+                {
+                        {-1,-1, 0,-9,-2,-2},
+                        {-2,-1,-6,-8,-2,-5},
+                        {-1,-1,-1,-2,-3,-4},
+                        {-1,-9,-2,-4,-4,-5},
+                        {-7,-3,-3,-2,-9,-9},
+                        {-1,-3,-1,-2,-4,-5}
+                };
         reverseArray(arr);
         reverseArray2(arr);
+        hourglassSum(twoDarr);
     }
 
     public static int[] reverseArray(int[] arr) {
@@ -31,4 +41,24 @@ public class ArrayManipulation {
         }
         return arr;
     }
+
+
+    public static int hourglassSum(int[][] arr) {
+        int sum = Integer.MIN_VALUE;
+
+        outer: for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 6; j++) {
+                if ((j + 2) >= 6) continue outer;
+
+                int tempSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1] +
+                        arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+                if (tempSum > sum) sum = tempSum;
+            }
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
 }
